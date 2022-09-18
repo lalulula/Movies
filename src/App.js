49e232a13dd87c86da96141fd9d28d1c100e4a09
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-18 19:58:42 +0900

    created dir for routers

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
 import {useState, useEffect} from "react";
-import Movie from "./components/Movie";
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  //ASYNC function으로 대체한 부분
-  // useEffect(()=>{
-  //   fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-  //   ).then((response)=>response.json())
-  //     .then((json)=>{
-  //       setMovies(json.data.movies)
-  //       setLoading(false)});
-  //   },[])  
-  const getMovies = async()=>{
-    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(()=>{
-    getMovies();
-  },[])
-  console.log(movies);
-
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) =>(
-            <Movie 
-              key={movie.id}
-              title = {movie.title}
-              summary = {movie.summary}
-              coverImg = {movie.medium_cover_image}
-              genres ={movie.genres}
-              />
-            ))}
-        </div>
-        )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
