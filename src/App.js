3ac45df6e2191ae5b6075eff258c01e18b220e60
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-18 19:04:37 +0900

    fetched API data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import userEvent from "@testing-library/user-event";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  //ASYNC function으로 대체한 부분
+  // useEffect(()=>{
+  //   fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+  //   ).then((response)=>response.json())
+  //     .then((json)=>{
+  //       setMovies(json.data.movies)
+  //       setLoading(false)});
+  //   },[])  
+  const getMovies = async()=>{
+    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(()=>{
+    getMovies();
+  },[])
+  console.log(movies);
+
+    
+  
   return (
     <div>
-      <h1>hello</h1>
+      {loading?<h1>Loading...</h1>:null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./styles.css";
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
+  // devtools console로그되는거 방지하기위해 일단 해둔거임
 );
 
 // reportWebVitals();
