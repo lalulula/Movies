78f12a1d02ad03d9abb65aa164431812a5f486c9
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-19 23:23:31 +0900

    getting details on display

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,38 @@
+import { useEffect, useState,  } from "react";
+import React, { Fragment } from 'react';
 import { useParams } from "react-router-dom";
+
 const Detail = () =>{
-  const movieId = useParams();
-  console.log(movieId);
-  return <h1>Detail</h1>
-}
+  const {movieId} = useParams();
+  const [movieDetails, setMovieDetails] = useState([]);
+
+  useEffect(()=>{
+    getDetails();
+  },[]); 
+
+  const getDetails = async () => {
+    const json =  await( await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`)).json();
+    setMovieDetails(json.data.movie);
+  };
+  console.log(movieDetails)
+
+  if(!movieDetails){
+    return <h1>Loading...</h1>
+  }else{
+
+    return (
+    <React.Fragment>
+      <h1>Detail</h1>
+      <span>{movieDetails.title}</span>
+      <img src={movieDetails.medium_cover_image} alt="title" />
+      <span>{movieDetails.year}</span>
+      <span>{movieDetails.url}</span>
+      <span>{movieDetails.rating}</span>
+      <span>{movieDetails.runtime}</span>
+      <span>{movieDetails.description_full}</span>      
+    </React.Fragment>
+  );
+  }
+  }
 
 export default Detail;
