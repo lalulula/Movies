3d592d5fc3e8da60ed258cc49c16d51d06c7bbe9
Yunah Kim <yunah.kim@stonybrook.edu>
2022-09-19 22:17:27 +0900

    used Router to reate path for home & details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import {useState, useEffect} from "react";
-
+import Home from "./routes/Home";
+import Detail from "./routes/Detail"
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+}from "react-router-dom";
 
 function App() {
-  return null;
+  return <Router>
+    <Switch>
+
+      <Route path="/movie">
+        <Detail />
+      </Route>
+
+      <Route path="/">
+        <Home />
+      </Route>
+
+    </Switch>
+  </Router>;
 }
 
 export default App;
